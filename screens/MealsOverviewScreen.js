494fcb7bed7c041b6b4f0ca4ff45b023113a4d2d
Christian Historillo <ianworks@Christians-MacBook-Pro.local>
2024-03-12 01:29:22 +0800

    Adding meals overview screen details and restyling the header screen

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,11 +1,49 @@
-import { View, Text, StyleSheet } from "react-native";
+import { useLayoutEffect } from "react";
 
-import { MEALS } from "../data/dummy-data";
+import { View, FlatList, Text, StyleSheet } from "react-native";
 
-function MealsOverviewScreen() {
+import { MEALS, CATEGORIES } from "../data/dummy-data";
+
+import MealItem from "../components/MealItem";
+
+function MealsOverviewScreen({ route, navigation }) {
+  const catId = route.params.categoryId;
+
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0;
+  });
+
+  useLayoutEffect(() => {
+    const categoryTitle = CATEGORIES.find(
+      (category) => category.id === catId
+    ).title;
+
+    navigation.setOptions({
+      title: categoryTitle,
+    });
+  }, [catId, navigation]);
+
+  function renderMealItem(itemData) {
+    const item = itemData.item;
+
+    const mealItem = {
+      title: item.title,
+      imageUrl: item.imageUrl,
+      affordability: item.affordability,
+      complexity: item.complexity,
+      duration: item.duration,
+    };
+
+    return <MealItem {...mealItem} />;
+  }
+
   return (
     <View style={styles.container}>
-      <Text> Meals Overview Screen </Text>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
     </View>
   );
 }
