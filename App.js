315ab89233c157f51bc40a608a6f7fd93e423efc
Christian Historillo <ianworks@Christians-MacBook-Pro.local>
2024-03-13 00:17:59 +0800

    Styling meals overview screen and adding icons on header

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Button, View } from "react-native";
 import CategoriesScreen from "./screens/CategoriesScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import MealsOverviewScreen from "./screens/MealsOverviewScreen";
+import MealDetailScreen from "./screens/MealDetailScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -35,6 +36,15 @@ export default function App() {
             //   return {
             //     title: catId,
             //   };
+            // }}
+          />
+          <Stack.Screen
+            name="MealDetail"
+            component={MealDetailScreen}
+            // options={{
+            //   headerRight: () => {
+            //     return <Button title="Tap me!" onPress={}/>;
+            //   },
             // }}
           />
         </Stack.Navigator>
